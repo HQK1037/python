dc01c214c7aa4de8d186f59db2eeb6ceb5e79fd3
HQK1037 <1819756403@qq.com>
2024-02-28 22:51:48 +0800

    bfs

--- hello.py
+++ hello.py
@@ -294,6 +294,27 @@ class Solution:
             cur = cur.next
         print()
 
+    # BFS: 队列  DFS：栈
+    def canReach(self, arr: List[int], start: int) -> bool:
+        if arr[start] == 0:
+            return True
+
+        n = len(arr)
+        used = {start}
+        q = deque([start])
+
+        while len(q) > 0:
+            u = q.popleft()
+            for v in [u + arr[u], u - arr[u]]:
+                if 0 <= v < n and v not in used:
+                    if arr[v] == 0:
+                        return True
+                    q.append(v)
+                    used.add(v)
+        
+        return False
+
+
 
 instance = Solution()
 
